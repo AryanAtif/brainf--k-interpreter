25b40b4a964c529b9fc669d18d82b256248f6cfe
Aryan Atif <aryanatif01@gmail.com>
2026-03-28 09:53:48 +0500

    Add error messages

--- main.c
+++ main.c
@@ -2,7 +2,16 @@
 
 int int main(int argc, char *argv[])
 {
-  if (argc <= 0) {printf ("Enter the")}
+  if (argc <= 1) {printf ("Enter the brainfuck code as an argument and try again.\n"); return 1;}
+  else if (argc > 2) {printf ("Entered too many arguments.\n"); return 1;}
+
+  
+
+  
+
+  
+
+  
   
   return 0;
 }
