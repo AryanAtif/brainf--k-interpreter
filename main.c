d3194514714ad8d0d101bcdffc8d75653b8a69ff
Aryan Atif <aryanatif01@gmail.com>
2026-04-23 10:22:05 +0500

    Free the memory before the program quits

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "interpreter.h"
 
@@ -7,5 +8,7 @@ int main(void)
   char *code = read_input();
   if (code == NULL) {printf ("Memory to the code array could not be allocated\n"); return 1;}
   init_interpreter (code);
+
+  free(code);
   return 0; 
 }
